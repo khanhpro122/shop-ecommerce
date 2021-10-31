846c63c9f094f65f09cd4af2d7fc1cc9c822f4d3
khanh <duykhanh.longcang@gmail.com>
2021-10-31 08:22:04 +0700

    start react

---
+++ fontend/src/data.js
@@ -0,0 +1,83 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Ao so mi',
+            category: 'Shirts',
+            image: '/images/product1.png',
+            price: 120,
+            brand: 'Ao',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Ao so mi',
+            category: 'Shirts',
+            image: '/images/product1.png',
+            price: 120,
+            brand: 'Ao',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '3',
+            name: 'Ao so mi',
+            category: 'Shirts',
+            image: '/images/product1.png',
+            price: 120,
+            brand: 'Ao',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '4',
+            name: 'Ao so mi',
+            category: 'Shirts',
+            image: '/images/product1.png',
+            price: 120,
+            brand: 'Ao',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '5',
+            name: 'Ao so mi',
+            category: 'Shirts',
+            image: '/images/product1.png',
+            price: 120,
+            brand: 'Ao',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '6',
+            name: 'Ao so mi',
+            category: 'Shirts',
+            image: '/images/product1.png',
+            price: 120,
+            brand: 'Ao',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '7',
+            name: 'Ao so mi',
+            category: 'Shirts',
+            image: '/images/product1.png',
+            price: 120,
+            brand: 'Ao',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+    ],
+};
+
+export default data
