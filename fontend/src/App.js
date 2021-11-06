26c66b99928b8c876f101fd3e44765f589991dee
khanh <duykhanh.longcang@gmail.com>
2021-11-06 17:27:03 +0700

    done

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import { signout } from './actions/userActions';
+import PrivateRoute from './components/PrivateRoute';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
+import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import OrderScreen from './screens/OrderScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import ProductScreen from './screens/ProductScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from './screens/SigninScreen';
@@ -29,7 +33,7 @@ function App() {
             <div>
                 <Link className="brand" to="/">Ecommerce</Link>
             </div>
-            <div>
+            <div className="flex">
                 <Link to="/cart">Cart
                 {cartItems.length > 0 && (
                   <span className="badge">{cartItems.length}</span>
@@ -42,15 +46,44 @@ function App() {
                         {userInfo.name} <i className="fa fa-caret-down"></i>
                       </Link>
                       <ul className="dropdown-content">
-                        <Link to="#signout" onClick={signoutHandler}>
-                          Sign Out
-                        </Link>
+                        <li>
+                          <Link to="/profile">User Profile</Link>
+                        </li>
+                        <li>
+                          <Link to="/orderhistory">Order History</Link>
+                        </li>
+                        <li>
+                          <Link to="#signout" onClick={signoutHandler}>
+                            Sign Out
+                          </Link>
+                        </li>
                       </ul>
                     </div>
                   ) : (
                     <Link to="/signin">Sign In</Link>
                   )
                 }
+                {userInfo && userInfo.isAdmin && (
+                  <div className="dropdown">
+                    <Link to="#admin">
+                      Admin <i className="fa fa-caret-down"></i>
+                    </Link>
+                    <ul class="dropdown-content">
+                        <li>
+                          <Link to="/dashboard">Dashboard</Link>
+                        </li>
+                        <li>
+                          <Link to="/productlist">Products</Link>
+                        </li>
+                        <li>
+                          <Link to="/orderlist">Orders</Link>
+                        </li>
+                        <li>
+                          <Link to="/userlist">Users</Link>
+                        </li>
+                    </ul>
+                  </div>
+                )}
             </div>
         </header>
         <main>
@@ -61,7 +94,11 @@ function App() {
           <Route path="/shipping" component={ShippingAddressScreen}></Route>
           <Route path="/payment" component={PaymentMethodScreen}></Route>
           <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+          <Route path ="/order/:id" component={OrderScreen}></Route>
+          <Route path ="/orderhistory" component={OrderHistoryScreen}></Route>
+          <PrivateRoute path ="/profile" component={ProfileScreen}></PrivateRoute>
           <Route path="/" component={HomeScreen} exact></Route>
+
         </main>
         <footer className="row center">
             All right reserved
